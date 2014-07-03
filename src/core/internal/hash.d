ca589d66a9ce253bd1bba1944f1ca18dc54a1ea0
k-hara <k.hara.pg@gmail.com>
2014-07-03 21:56:31 +0900

    Do not ensure `pure` for struct toHash() method

--- src/core/internal/hash.d
+++ src/core/internal/hash.d
@@ -101,7 +101,6 @@ size_t hashOf(T)(auto ref T val, size_t seed = 0) if (!is(T == enum) && is(T V :
 }
 
 // struct or union hash
-pure
 size_t hashOf(T)(auto ref T val, size_t seed = 0) if (!is(T == enum) && (is(T == struct) || is(T == union)))
 {
     static if (is(typeof(val.toHash()) == size_t))
